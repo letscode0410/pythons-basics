b8300cb650e9bd704b1bab406d61c2e30508c2af
Ravi Kumar P <ravikumar.p536@gmail.com>
2022-11-13 11:59:16 +0530

    added new files

---
+++ python_dict.py
@@ -0,0 +1,21 @@
+my_dict={'key1':'value1','key2':'value2'}
+
+print(my_dict)
+print(my_dict['key1'])
+
+price_lookup = {'apple':2.99,'mango':56.98,'milk':45.6}
+
+print(price_lookup)
+print(price_lookup['milk'])
+
+d= {'k1':123,'k2':[1,2,3],'k3':{'insideK3':109}}
+
+print(d)
+
+print(d['k3']['insideK3'])
+
+print(d['k2'][2])
+
+print(d.keys())
+print(d.values())
+print(d.items())
---
+++ python_tuple.py
@@ -0,0 +1,14 @@
+t = (1,2,3,1,3,1)
+
+print(type(t))
+
+l= [2,3,4,5]
+print(type(l))
+
+print(t)
+
+print(t.count(1))
+
+print(t.index(3))
+
+#tuples doesnt support assignment
