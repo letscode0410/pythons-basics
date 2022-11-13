6baca9cf7327e55902a2d09c4c1e79cab61545ad
Ravi Kumar P <ravikumar.p536@gmail.com>
2022-11-13 12:17:00 +0530

    set example is added

---
+++ python_set.py
@@ -0,0 +1,16 @@
+set_new = set()
+print(set_new)
+
+set_new.add(1)
+
+set_new.add(2)
+
+set_new.add(1)
+
+set_new.add("ravi kumar")
+
+print(set_new)
+
+#{'r', 'a', 'k', 'u', 'v', 'm', 'i'}
+set_new=set("ravikumar")
+print(set_new)
