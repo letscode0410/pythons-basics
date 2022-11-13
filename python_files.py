7e247e88eb8ae1e6000db817c36daa502cc8a0dd
Ravi Kumar P <ravikumar.p536@gmail.com>
2022-11-13 13:33:29 +0530

    python_files updated

--- python_files.py
+++ python_files.py
@@ -2,3 +2,14 @@ file_data=open("sample.txt")
 data=file_data.read()
 print(data)
 
+#nothing will be read
+data=file_data.read()
+print(data)
+
+# now it will read
+file_data.seek(0)
+data=file_data.read()
+print(data)
+
+file_data.seek(0)
+print(file_data.readlines())
