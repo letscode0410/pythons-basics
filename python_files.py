0784313d570c1d94071c7e160fc07e128c73b98f
Ravi Kumar P <ravikumar.p536@gmail.com>
2022-11-13 13:57:47 +0530

    update to python_files

--- python_files.py
+++ python_files.py
@@ -13,3 +13,39 @@ print(data)
 
 file_data.seek(0)
 print(file_data.readlines())
+
+file_data.close()
+
+print("**************separator****************")
+with open("sample.txt") as sample_file:
+    print(sample_file.readlines())
+    #no need to close
+
+with open("sample.txt",mode='a') as f:
+    f.write("\nfifth line by write")
+
+with open("sample.txt",mode='r') as sample_file:
+    print(sample_file.readlines())
+
+#r = read w=write (overwrite) a= append  r+ and w+
+
+
+#Basic Practice:
+
+#http://codingbat.com/python
+
+#More Mathematical (and Harder) Practice:
+
+#https://projecteuler.net/archives
+
+#List of Practice Problems:
+
+#http://www.codeabbey.com/index/task_list
+
+#A SubReddit Devoted to Daily Practice Problems:
+
+#https://www.reddit.com/r/dailyprogrammer
+
+#A very tricky website with very few hints and touch problems (Not for beginners but still interesting)
+
+#http://www.pythonchallenge.com/
