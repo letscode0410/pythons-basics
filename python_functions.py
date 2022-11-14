192671b444b369375ae9ba900d360a541c9a8549
Ravi Kumar P <ravikumar.p536@gmail.com>
2022-11-14 15:36:41 +0530

    functions update

--- python_functions.py
+++ python_functions.py
@@ -34,7 +34,15 @@ def factorial(num):
         fact *=i
     return fact
 
-print(factorial(6))\
+print(factorial(6))
+
+
+#even number
+
+def even_check(number):
+    return number%2 ==0
+
+print("is even ",even_check(5))
 
 
 #optional argument, and optional arguments should always followed by non -optional arguments 
@@ -50,6 +58,51 @@ def multiply(a,b=3,c=1):
 
 print(multiply(5,2,5))
 print(multiply(6,2))
+
+#Check if any number in a list is even
+
+def check_if_even_list(num_list):
+    for num in num_list:
+        if(num%2==0):
+            return True
+        else:
+            pass
+    return False
+
+print("check_if_even_list([3,5,6,7])",check_if_even_list([3,5,6,7]))
+print("check_if_even_list([3,5,9,7])",check_if_even_list([3,5,9,7]))
+
+
+#Return all even numbers in a list
+def get_even_list(num_list):
+    even_numbers=[]
+    for num in num_list:
+        if num%2==0:
+            even_numbers.append(num)
+        else:
+            pass
+    return even_numbers
+
+print("get_even_list(range(1,11))",get_even_list(range(1,11)))
+
+#The employee of the month function will return both 
+# the name and number of hours worked for the top 
+# performer (judged by number of hours worked).    
+work_hours = [('ravi',100),('kumar',400),('reddy',800)]
+
+def get_top_performer(work_hours):
+    max_hours =0
+    employee_of_month=""
+    for employee,hours in work_hours:
+        if hours > max_hours:
+            max_hours=hours
+            employee_of_month=employee
+        else:
+            pass
+    return (employee_of_month,max_hours)
+
+print("get_top_performer(work_hours)",get_top_performer(work_hours))
+
 
 #Non-key values arguments
 def  example_nonkeyed_args(*argv):
@@ -68,5 +121,9 @@ def example_of_kwargs(**kwargs):
         print("key :",k," value :",v)
 
 example_of_kwargs(host="localhost",port=9091,pswd="xxx")
+
+
+
+
 
 
