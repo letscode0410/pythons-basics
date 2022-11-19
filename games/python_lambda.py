0c10371392936a439045f7a8dbf51fc51aad1365
Ravi Kumar P <ravikumar.p536@gmail.com>
2022-11-19 16:43:55 +0530

    lambda

--- games/python_lambda.py
+++ games/python_lambda.py
@@ -5,3 +5,23 @@ data = map(lambda x:"even" if x%2==0 else "odd",list1)
 
 for i in data:
     print(i)
+
+list2 = [1,2,3,4]
+list3 = [1,2,3,4]
+
+data1= list(map(lambda x,y:x+y,list3,list2))
+print(data1)
+
+
+
+###how to use reduce####
+import functools
+
+list_x =[1,2,3,4,5,6]
+res=functools.reduce(lambda x,y: x+y,list_x)
+print(res)
+
+
+### filter
+filtered_data = filter(lambda x:x%2==0,list_x)
+print(list(filtered_data))
