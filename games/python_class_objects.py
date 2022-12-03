94d76674f53a0f31fbc1a8bc561b750632bfc799
Ravi Kumar P <ravikumar.p536@gmail.com>
2022-12-03 22:06:15 +0530

    pandas added

--- games/python_class_objects.py
+++ games/python_class_objects.py
@@ -1,10 +1,13 @@
 #how to create a class in python
 class Employee:
 
+    count =0
+
     #constructor of the class
     def __init__(self,name,salary):
         self.name = name
         self.salary = salary
+        Employee.count +=1
     
     def getEmployeeDetails(self):
         return f"Name : {self.name} Salary : {self.salary}"
@@ -19,4 +22,7 @@ class Employee:
 emp = Employee("ravi",10000)
 print(emp.getEmployeeDetails())
 print(emp)
-print(len(emp))
+#print(len(emp))
+
+emp1 = Employee("ravi",10000)
+print(emp1.count)
